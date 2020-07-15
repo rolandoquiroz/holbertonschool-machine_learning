afa1b6f31750f28b41afc3f137f75738774b8cee
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-15 17:51:48 -0500

    Adds Tasks 10 to 12 with pep8 checked

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''np_shape: Calculates the shape of a numpy.ndarray'''
+
+
+def np_shape(matrix):
+    '''np_shape: Calculates the shape of a numpy.ndarray'''
+    return matrix.shape
---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''np_transpose(matrix): that transposes matrix'''
+
+
+def np_transpose(matrix):
+    '''np_transpose(matrix): that transposes matrix'''
+    return matrix.transpose()
