dd1ddf353a01c6cc20887ddbd1a82cd74e6294a2
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-15 18:31:46 -0500

    Adds Tasks 13 to 14 with pep8 checked

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""np_matmul: Performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """np_matmul: Performs matrix multiplication"""
+    return np.matmul(mat1, mat2)
