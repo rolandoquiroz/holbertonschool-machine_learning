cc2ab653ab6499bcf96019af38dc6cfa43083dbc
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-15 01:36:46 -0500

    Task 7: Edge case 1x1 checked

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -10,8 +10,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
             for j in range(len(mat2)):
                 new_mat.append(mat2[j])
             return new_mat
+
     if axis == 1:
-        if len(mat1[0]) == len(mat2):
+        if len(mat1) == len(mat2):
             new_mat = [i[:] for i in mat1]
             for j in range(len(mat2)):
                 new_mat[j].extend(mat2[j])
