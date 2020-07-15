a58a49e835f0db32074c415f81c6d77c13197c7d
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-15 00:33:36 -0500

    This commit uploads Tasks 7 version 0.0

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,8 +5,14 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     '''cat_matrices2D: Concatenates two matrices along a specific axis'''
     if axis == 0:
-        new_mat = mat1[:]
-        new_mat.append(mat2[0])
-    return new_mat
-    
-
+        if len(mat1[0]) == len(mat2[0]):
+            new_mat = [i[:] for i in mat1]
+            for j in range(len(mat2)):
+                new_mat.append(mat2[j])
+            return new_mat
+    if axis == 1:
+        if len(mat1[0]) == len(mat2):
+            new_mat = [i[:] for i in mat1]
+            for j in range(len(mat2)):
+                new_mat[j].extend(mat2[j])
+            return new_mat
