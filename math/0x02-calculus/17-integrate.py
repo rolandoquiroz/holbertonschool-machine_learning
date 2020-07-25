6521ee2b965d9e0d0deaddf52804e43c1dcb570f
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-25 16:05:31 -0500

    Add Task 17.

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -31,7 +31,6 @@ def poly_integral(poly, C=0):
         return None
     if poly == [0]:
         return [C]
-    integral_coefficients = []
     integral_coefficients = [C]
     for i in range(len(poly)):
         if type(poly[i]) not in [int, float]:
