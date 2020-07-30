4223b612ff5006fca15515ae4371f9d7e8a8d6f9
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-30 15:42:51 -0500

    Task 3 added.

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+"""Exponential class module"""
+
+
+class Exponential:
+    """Exponential probability distribution class"""
+
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """Exponential object attributes initialization
+            Args:
+                data (list): A list of the data to be used to estimate
+                    the distribution
+                lambtha (float): Expected number of occurences in a given
+                    time frame
+            Raises:
+                TypeError: If data is not a list
+                ValueError: If data does not contain at least two data points
+                            If lambtha is not a positive value
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                for value in data:
+                    if type(value) not in [float, int]:
+                        raise ValueError("data must contain multiple values")
+                self.lambtha = len(data)/sum(data)
