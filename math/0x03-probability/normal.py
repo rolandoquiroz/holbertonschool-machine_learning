84e24468b5373a38b082dbe021a26c4f59d706b1
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-30 19:35:13 -0500

    Adds normal.py

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -37,3 +37,47 @@ class Normal:
                 self.mean = sum(data)/len(data)
                 self.stddev = (sum((x - self.mean)**2 for x in data) /
                                len(data))**0.5
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value
+
+            Args:
+                x (float): The x-value
+            Returns:
+                (float): The z-score of x
+        """
+        return (x - self.mean)/self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score
+
+            Args:
+                z (float): The z-score
+            Returns:
+                (float): The x-value of z
+        """
+        return (z*self.stddev + self.mean)
+
+    def pdf(self, x):
+        """Calculates the value of the Normal PDF for a given x-value
+
+            Args:
+                x (float): The x-value
+            Returns:
+                (float): PDF value for x
+        """
+        return ((Normal.e**(-0.5*((x-self.mean)/self.stddev)**2)) /
+                (self.stddev*(2*Normal.π)**0.5))
+
+    def cdf(self, x):
+        """Calculates the value of the Normal CDF for a given x-value
+
+            Args:
+                x (float): The x-value
+            Returns:
+                (float): CDF value for x
+        """
+        x0 = (x-self.mean)/((2 ** 0.5)*self.stddev)
+        erfx = ((2/(Normal.π)**0.5)*(x0-((x0**3)/3) +
+                                     ((x0**5)/10)-((x0**7)/42)+((x0**9)/216)))
+        return ((1 + erfx) / 2)
