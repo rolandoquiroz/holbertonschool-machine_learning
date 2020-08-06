38a9a301b4f2f25c20fea8e3dc5d5ea615371781
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-06 16:38:20 -0500

    Task 1, 2 added

--- supervised_learning/0x00-binary_classification/2-neuron.py
+++ supervised_learning/0x00-binary_classification/2-neuron.py
@@ -1,1 +1,58 @@
 #!/usr/bin/env python3
+"""Neuron class module"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Neuron object attributes initialization
+
+            Args:
+                nx (int): Number of input features to the neuron
+
+            Raises:
+                TypeError: If nx is not an integer
+                ValueError: If nx is less than 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(0, 1, (1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        W getter method
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        xb getter method
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        A getter method
+        """
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron
+
+            Args:
+                nx (int): Number of input features to the neuron
+                m (int): Number of examples
+                X (numpy.ndarray): Input data with shape (nx, m)
+        """
+        output = np.matmul(self.__W, X) + self.__b
+        self.__A = 1/(1 + np.exp(-output))
+        return self.__A
