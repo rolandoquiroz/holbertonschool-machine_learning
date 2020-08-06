6a5100e5d23e3b0ba2400170fcfef4b349cc0389
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-06 13:47:33 -0500

    Task 0 added

--- supervised_learning/0x00-binary_classification/0-neuron.py
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+"""Neuron class module"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Neuron object attributes initialization
+
+            Args:
+                nx (int): Number of input features to the neuron
+
+            Raises:
+                TypeError: If nx is not an integer
+                ValueError: If nx is less than 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.normal(0, 1, (1, nx))
+        self.b = 0
+        self.A = 0
