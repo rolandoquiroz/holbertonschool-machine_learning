e156de6a83b305a0e7effd79cd33fa201f03dac8
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-08 14:05:33 -0500

    Adds task 8

--- supervised_learning/0x00-binary_classification/8-neural_network.py
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+"""NeuralNetwork class module"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """NeuralNetwork defines a neural network with one hidden layer
+        performing binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """NeuralNetwork object attributes initialization
+
+        Args:
+            nx (int): Number of input features to the NeuralNetwork
+            nodes (int) : Number of nodes found in the hidden layer
+
+        Raises:
+            TypeError: If nx is not an integer or if nodes is not an integer
+            ValueError: If nx is less than 1 or if nodes is less than 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.normal(0, 1, (1, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(0, 1, (1, nodes))
+        self.b2 = 0
+        self.A2 = 0
