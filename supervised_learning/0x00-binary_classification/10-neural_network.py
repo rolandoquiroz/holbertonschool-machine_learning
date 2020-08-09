6169e3865d8a678f6bb50132945a1387e61d4acc
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-08 19:15:09 -0500

    Adds Task 10

--- supervised_learning/0x00-binary_classification/10-neural_network.py
+++ supervised_learning/0x00-binary_classification/10-neural_network.py
@@ -1,1 +1,112 @@
 #!/usr/bin/env python3
+"""NeuralNetwork class module"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """NeuralNetwork defines a neural network with one hidden layer
+        performing binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """NeuralNetwork object attributes initialization
+
+        Args:
+            nx (int): Number of input features to the NeuralNetwork
+            nodes (int) : Number of nodes found in the hidden layer
+
+        Attributes:
+            W1: Weights vector for the hidden layer. Upon instantiation,
+                it should be initialized using a random normal distribution.
+            b1: The bias for the hidden layer. Upon instantiation,
+                it should be initialized with 0’s.
+            A1: The activated output for the hidden layer.
+                Upon instantiation, it should be initialized to 0.
+            W2: The weights vector for the output neuron. Upon instantiation,
+                it should be initialized using a random normal distribution.
+            b2: The bias for the output neuron. Upon instantiation,
+                it should be initialized to 0.
+            A2; The activated output for the output neuron (prediction).
+                Upon instantiation, it should be initialized to 0.
+
+        Raises:
+            TypeError: If nx is not an integer or if nodes is not an integer
+            ValueError: If nx is less than 1 or if nodes is less than 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.normal(0, 1, (nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(0, 1, (1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """
+        W1 getter method
+        """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """
+        b1 getter method
+        """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """
+        A getter method
+        """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """
+        W getter method
+        """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """
+        b getter method
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """
+        A getter method
+        """
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network
+            using sigmoid activation function
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Input data with shape (nx, m)
+
+        Returns
+        -------
+        float
+            The forward propagation of the neuron using sigmoid
+            activation function
+        """
+        z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-z1))
+        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-z2))
+        return self.__A1, self.__A2
