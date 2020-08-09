e45c9f737a1a0db941d57c37364b4d8c10eedf72
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-09 15:22:20 -0500

    Add task 16

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -39,19 +39,19 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.nx = nx
         if type(layers) is not list or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
+        self.nx = nx
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
         for i in range(self.L):
-            if type(layers[i]) is not int or layers[i] < 1:
+            if (type(layers[i]) is not int or layers[i] <= 0):
                 raise ValueError("layers must be a list of positive integers")
             self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
             if i == 0:
-                self.weights["W{}".format(i + 1)] = np.random.randn(layers[i],
-                                                     nx)*np.sqrt(2/nx)
+                self.weights["W{}".format(i + 1)] = (np.random.randn(layers[i],
+                                                     nx)*np.sqrt(2/nx))
             else:
                 self.weights["W{}".format(i + 1)] = (np.random.randn(layers[i],
                                                      layers[i - 1]) *
