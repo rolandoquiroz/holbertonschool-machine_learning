eadb9352df20d8516a22fb3c5088d83a4ceb141b
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-16 01:16:51 +0000

    [Upload] Task 1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""0-create_layer module
+contains the function create_layer
+"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """Creates the layers for the neural network.
+
+    Args:
+        prev: `Tensor`, is the tensor output of the previous layer.
+        n: `int`, is the number of nodes in the layer to create.
+        activation: is the activation function that the layer should use.
+
+    Returns:
+        `Tensor`, the tensor output of the layer.
+    """
+    model = tf.layers.Dense(units=n, activation=activation, name="layer")
+    y = model(prev)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    return y
