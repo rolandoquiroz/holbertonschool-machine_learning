e6bd6675a41dda9fb6656b56c7deefde9ca9cb6e
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-28 02:17:53 -0500

    [Fix] Task 2: Typo on l2_reg_loss definition

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -16,6 +16,6 @@ def l2_reg_cost(cost):
         J: `Tensor`, that contains the cost of the
             network accounting for L2 regularization
     """
-    l2_reg_loss = tf.losses.get_regularization_loss()
+    l2_reg_loss = tf.losses.get_regularization_losses()
     J = cost + l2_reg_loss
     return J
