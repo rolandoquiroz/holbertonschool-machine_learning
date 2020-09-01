52dd7140bed3667d952b38b79d3b1484f66bb086
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-01 16:27:02 +0000

    [Fix] keyword argument passed data and labels passed by position

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -32,7 +32,7 @@ def train_model(network, data, labels, batch_size,
         history: `History`, The History object generated after
             training the model
     """
-    history = network.fit(x=data, y=labels,
+    history = network.fit(data, labels,
                           batch_size=batch_size,
                           epochs=epochs,
                           validation_data=validation_data,
