252d00a1a72fc2f25446654dec589e217ffcb51e
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-01 19:29:17 +0000

    [Add] Task 9

--- supervised_learning/0x06-keras/9-model.py
+++ supervised_learning/0x06-keras/9-model.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 """
-module
-contains the function
+module 9-model
+contains the functions save_model and load_model
 """
 import tensorflow.keras as K
 
 
-def 
+def save_model(network, filename):
+    """Saves an entire keras model
+
+    Args:
+        network: the model to save
+        filename: `str`, is the path of the file
+            that the model should be saved to
+
+    Returns:
+        None
+    """
+    network.save(filename)
+    return None
+
+
+def load_model(filename):
+    """Loads an entire keras model
+
+    Args:
+        filename: `str`,  is the path of the file
+            that the model should be loaded from
+
+    Returns:
+        network: the loaded model
+    """
+    network = K.models.load_model(filename)
+    return network
