4df3899120495404f433c0c6c00ea2533cc7bfbe
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-04 03:55:36 +0000

    Task 0 added axis sum setted right

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -32,5 +32,6 @@ def convolve_grayscale_valid(images, kernel):
 
     for i in range(output_width):
         for j in range(output_height):
-            output[:, i, j] = (kernel * images[:, i: i + kh, j: j + kw]).sum(axis=(1, 2))
+            output[:, i, j] = (kernel * images[:, i: i + kh, j: j + kw]).\
+                sum(axis=(2, 1))
     return output
