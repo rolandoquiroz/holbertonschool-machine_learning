12242f86a895cb0174063e4f0618310f64b68318
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-04 07:23:08 +0000

    Add Task 1

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -20,18 +20,17 @@ def convolve_grayscale_valid(images, kernel):
             kh: `int`, is the height of the kernel
             kw: `int`, is the width of the kernel
     Returns:
-        a numpy.ndarray containing the convolved images
+        output: `numpy.ndarray` containing the convolved images
     """
     m, h, w = images.shape[0], images.shape[1], images.shape[2]
     kh, kw = kernel.shape[0], kernel.shape[1]
 
-    output_height = h - kh + 1
-    output_width = w - kw + 1
+    output_h = h - kh + 1
+    output_w = w - kw + 1
 
-    output = np.zeros((m, output_height, output_width))
-
-    for i in range(output_width):
-        for j in range(output_height):
+    output = np.zeros((m, output_h, output_w))
+    for i in range(output_w):
+        for j in range(output_h):
             output[:, j, i] = ((kernel * images[:, j: j + kh, i: i + kw]).
                                sum(axis=(1, 2)))
     return output
--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,1 +1,43 @@
 #!/usr/bin/env python3
+"""
+module 1-convolve_grayscale_same
+contains function convolve_grayscale_same
+"""
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """Performs a valid convolution on grayscale images
+
+    Args:
+        images: `numpy.ndarray` with shape (m, h, w)
+            containing multiple grayscale images
+            m: `int`, is the number of images
+            h: `int`, is the height in pixels of the images
+            w: `int`, is the width in pixels of the images
+        kernel: `numpy.ndarray` with shape (kh, kw)
+            containing the kernel for the convolution
+            kh: `int`, is the height of the kernel
+            kw: `int`, is the width of the kernel
+    Returns:
+         containing the convolved images
+    """
+    m, h, w = images.shape[0], images.shape[1], images.shape[2]
+    kh, kw = kernel.shape[0], kernel.shape[1]
+
+    output_h = h - kh + 1
+    output_w = w - kw + 1
+
+    ph = ((kh - 1) // 2) if kh % 2 else (kh // 2)
+    pw = ((kw - 1) // 2) if kw % 2 else (kw // 2)
+
+    padded_images = np.pad(images, [(0, 0), (ph, ph), (pw, pw)], 'constant',
+                           constant_values=0)
+
+    output = np.zeros((m, output_h, output_w))
+
+    for i in range(output_w):
+        for j in range(output_h):
+            output[:, j, i] = ((kernel * padded_images[:,
+                               j: j + kh, i: i + kw]).sum(axis=(1, 2)))
+    return output
