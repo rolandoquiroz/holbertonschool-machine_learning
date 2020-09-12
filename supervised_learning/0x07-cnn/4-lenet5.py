8a97751dbb2302e1a4312de186e90971ee5f6409
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-12 01:57:37 +0000

    Task 5 added

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -25,48 +25,33 @@ def lenet5(x, y):
     """
     initializer = tf.contrib.layers.variance_scaling_initializer()
 
-    convolutional_layer_1 = tf.layers.Conv2D(filters=6,
-                                             kernel_size=(5, 5),
-                                             padding='same',
-                                             activation=tf.nn.relu,
-                                             kernel_initializer=initializer,
-                                             name='convolutional_layer_1')(x)
+    conv_layer_1 = tf.layers.Conv2D(filters=6,
+                                    kernel_size=(5, 5),
+                                    padding='same',
+                                    activation=tf.nn.relu,
+                                    kernel_initializer=initializer)(x)
 
-    max_pooling_layer_2 = tf.\
-        layers.MaxPooling2D(pool_size=(2, 2),
-                            strides=(2, 2),
-                            name='max_pooling_layer_2')(convolutional_layer_1)
+    p_layer_2 = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                       strides=(2, 2))(conv_layer_1)
 
-    convolutional_layer_3 = tf.\
-        layers.Conv2D(filters=16,
-                      kernel_size=(5, 5),
-                      padding='valid',
-                      activation=tf.nn.relu,
-                      kernel_initializer=initializer,
-                      name='convolutional_layer_3')(max_pooling_layer_2)
+    conv_layer_3 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
+                                    padding='valid', activation=tf.nn.relu,
+                                    kernel_initializer=initializer)(p_layer_2)
 
-    max_pooling_layer_4 = tf.\
-        layers.MaxPooling2D(pool_size=(2, 2),
-                            strides=(2, 2),
-                            name='max_pooling_layer_4')(convolutional_layer_3)
+    p_layer_4 = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                       strides=(2, 2))(conv_layer_3)
 
     # Flattening between conv and dense layers
-    flatten_layer = tf.layers.Flatten()(max_pooling_layer_4)
+    flatten_layer = tf.layers.Flatten()(p_layer_4)
 
-    fully_connected_layer_5 = tf.\
-        layers.Dense(units=120,
-                     activation=tf.nn.relu,
-                     kernel_initializer=initializer,
-                     name='fully_connected_layer_5')(flatten_layer)
+    fc_layer_5 = tf.layers.Dense(units=120, activation=tf.nn.relu,
+                                 kernel_initializer=initializer)(flatten_layer)
 
-    fully_connected_layer_6 = tf.\
-        layers.Dense(units=84,
-                     kernel_initializer=initializer,
-                     name='fully_connected_layer_6')(fully_connected_layer_5)
+    fc_layer_6 = tf.layers.Dense(units=84,
+                                 kernel_initializer=initializer)(fc_layer_5)
 
     output = tf.layers.Dense(units=10,
-                             kernel_initializer=initializer,
-                             name='output')(fully_connected_layer_6)
+                             kernel_initializer=initializer)(fc_layer_6)
 
     loss = tf.losses.softmax_cross_entropy(y, output)
 
