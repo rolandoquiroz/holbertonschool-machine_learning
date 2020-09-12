8b9e0e19a96b25da83e5acd28103082dfb07a23f
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-12 02:16:57 +0000

    Task 5 added

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -17,7 +17,7 @@ def lenet5(x, y):
         for the network
 
     Returns:
-        a tensor for the softmax activated output
+        a tensor for the softmax activated output_layer
         a training operation that utilizes Adam optimization
             (with default hyperparameters)
         a tensor for the loss of the netowrk
@@ -25,43 +25,45 @@ def lenet5(x, y):
     """
     initializer = tf.contrib.layers.variance_scaling_initializer()
 
+    activation = tf.nn.relu
+
     conv_layer_1 = tf.layers.Conv2D(filters=6,
-                                    kernel_size=(5, 5),
+                                    kernel_size=5,
                                     padding='same',
-                                    activation=tf.nn.relu,
+                                    activation=activation,
                                     kernel_initializer=initializer)(x)
 
-    p_layer_2 = tf.layers.MaxPooling2D(pool_size=(2, 2),
-                                       strides=(2, 2))(conv_layer_1)
+    p_layer_2 = tf.layers.MaxPooling2D(pool_size=[2, 2],
+                                       strides=2)(conv_layer_1)
 
-    conv_layer_3 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
-                                    padding='valid', activation=tf.nn.relu,
+    conv_layer_3 = tf.layers.Conv2D(filters=16, kernel_size=5,
+                                    padding='valid', activation=activation,
                                     kernel_initializer=initializer)(p_layer_2)
 
-    p_layer_4 = tf.layers.MaxPooling2D(pool_size=(2, 2),
-                                       strides=(2, 2))(conv_layer_3)
+    p_layer_4 = tf.layers.MaxPooling2D(pool_size=[2, 2],
+                                       strides=2)(conv_layer_3)
 
     # Flattening between conv and dense layers
     flatten_layer = tf.layers.Flatten()(p_layer_4)
 
-    fc_layer_5 = tf.layers.Dense(units=120, activation=tf.nn.relu,
+    fc_layer_5 = tf.layers.Dense(units=120, activation=activation,
                                  kernel_initializer=initializer)(flatten_layer)
 
     fc_layer_6 = tf.layers.Dense(units=84,
                                  kernel_initializer=initializer)(fc_layer_5)
 
-    output = tf.layers.Dense(units=10,
-                             kernel_initializer=initializer)(fc_layer_6)
+    output_layer = tf.layers.Dense(units=10,
+                                   kernel_initializer=initializer)(fc_layer_6)
 
-    loss = tf.losses.softmax_cross_entropy(y, output)
+    loss = tf.losses.softmax_cross_entropy(y, output_layer)
 
-    y_pred = tf.nn.softmax(output)
+    y_pred = tf.nn.softmax(output_layer)
 
     train_op = tf.train.AdamOptimizer().minimize(loss)
 
     y_tag = tf.argmax(y, 1)
 
-    y_pred_tag = tf.argmax(y_pred, 1)
+    y_pred_tag = tf.argmax(output_layer, 1)
 
     equality = tf.equal(y_tag, y_pred_tag)
 
