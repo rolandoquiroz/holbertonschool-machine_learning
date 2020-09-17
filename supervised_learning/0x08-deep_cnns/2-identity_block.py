c843ff963a7a008b4b40ed0278e397dba05d057c
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-17 15:19:53 +0000

    Task 2 added

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -1,9 +1,62 @@
 #!/usr/bin/env python3
 """
-module 
-that contains the function 
+0-inception_block module
+that contains the function inception_block
 """
+import tensorflow.keras as K
 
 
+def identity_block(A_prev, filters):
+    """Builds an identity block as described in Deep Residual Learning for
+        Image Recognition (2015): https://arxiv.org/pdf/1512.03385.pdf
 
-def
+        Args:
+            - A_prev is the output from the previous layer
+            - filters: is a tuple or list containing F11, F3, F12,
+                respectively:
+                - F11: is the number of filters in the 1x1 convolution
+                - F3: is the number of filters in the 1x1 convolution before
+                    the 3x3 convolution
+                - F12: is the number of filters in the 1x1 convolution before
+                    the 3x3 convolution
+            - All convolutions inside the block should be followed by batch
+                normalization along the channels axis and a rectified linear
+                activation (ReLU), respectively.
+            - All weights should use he normal initialization
+
+        Returns:
+            - the activated output of the identity block
+    """
+    F11, F3, F12 = filters
+
+    initializer = K.initializers.he_normal(seed=None)
+
+    F11_conv = K.layers.Conv2D(filters=F11,
+                               kernel_size=(1, 1),
+                               padding='same',
+                               kernel_initializer=initializer)(A_prev)
+
+    BN_F11_conv = K.layers.BatchNormalization(axis=3)(F11_conv)
+
+    relu_BN_F11_conv = K.layers.Activation('relu')(BN_F11_conv)
+
+    F3_conv = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
+                              padding='same',
+                              kernel_initializer=initializer)(relu_BN_F11_conv)
+
+    BN_F3_conv = K.layers.BatchNormalization(axis=3)(F3_conv)
+
+    relu_BN_F3_conv = K.layers.Activation('relu')(BN_F3_conv)
+
+    F12_conv = K.layers.Conv2D(filters=F12,
+                               kernel_size=(1, 1),
+                               padding='same',
+                               kernel_initializer=initializer)(relu_BN_F3_conv)
+
+    BN_F12_conv = K.layers.BatchNormalization(axis=3)(F12_conv)
+
+    addition = K.layers.Add()([BN_F12_conv, A_prev])
+
+    output = K.layers.Activation('relu')(addition)
+
+    return output
