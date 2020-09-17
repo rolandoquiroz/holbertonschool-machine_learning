6c376c6061a338cd7755890b8877a865987bb7f6
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-17 07:07:01 +0000

    Tasx 1 doc added

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 """
-module 
-that contains the function 
+1-inception_network module
+that contains the function inception_network
 """
+import tensorflow.keras as K
+inception_block = __import__('0-inception_block').inception_block
 
 
+def inception_network():
+    """Builds the inception network as described in Going Deeper with
+        Convolutions (2014): https://arxiv.org/pdf/1409.4842.pdf
 
-def
+        Returns:
+            the keras model
+    """
