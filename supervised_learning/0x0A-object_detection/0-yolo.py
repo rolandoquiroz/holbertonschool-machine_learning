f9ab8dfa8bee4a1c058dbedf5f36f0a80e474849
rolandoquiroz <rolandoquiroz@gmail.com>
2020-10-03 20:37:34 +0000

    Task 0 added

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -15,28 +15,6 @@ class Yolo:
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """
         class constructor
-
-        Args:
-          model_path: `str`, the path to where a Darknet Keras model is stored
-          classes_path: `str`, the path to where the list of class names used
-            for the Darknet model, listed in order of index, can be found
-          class_t: `float` representing the box score threshold for the initial
-            filtering step
-          nms_t: `float` representing the IOU threshold for non-max suppression
-          anchors: `numpy.ndarray` of shape (outputs, anchor_boxes, 2)
-            containing all of the anchor boxes:
-              outputs: `int`, is the number of outputs (predictions) made
-                by the Darknet model
-              anchor_boxes: `int`, is the number of anchor boxes used for each
-                prediction
-              2 => [anchor_box_width, anchor_box_height]
-
-        Public instance attributes:
-          model: the Darknet Keras model
-          class_names: a list of the class names for the model
-          class_t: the box score threshold for the initial filtering step
-          nms_t: the IOU threshold for non-max suppression
-          anchors: the anchor boxes
         """
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
