677074ca5e32ba12b3255c3a46d4c7cd792db0f7
rolandoquiroz <rolandoquiroz@gmail.com>
2020-10-21 15:49:25 +0000

    Adds Task 0 and 1

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -1,35 +1,20 @@
 #!/usr/bin/env python3
 """
 0-determinant module
-contains determinant function and recursive_determinant function
+contains functions: matmin and determinant
 """
 
 
-def recursive_determinant(M, det=0):
-    """Calculates the determinant of a matrix
+def matmin(m, i, j):
+    """Slices matrix for minor calculation
     Args:
-        M (list): list of lists whose determinant should be calculated
+        matrix (list): list of lists whose minor should be calculated
+        i (int):
+        j (int):
     Returns:
-        det (float): the determinant of matrix
+        (list): the sliced matrix for minor calculation
     """
-    if len(M) == 1:
-        return M[0][0]
-    if len(M) == 2:
-        return M[0][0] * M[1][1] - M[1][0] * M[0][1]
-
-    i = list(range(len(M)))
-    for cur_col in i:
-        copied_matrix = [row[:] for row in M]
-        sub_M = copied_matrix[1:]
-        j = len(sub_M)
-
-        for i in range(j):
-            sub_M[i] = sub_M[i][0:cur_col] + sub_M[i][cur_col+1:]
-
-        sub_det = recursive_determinant(sub_M)
-        det += ((-1) ** (cur_col % 2)) * M[0][cur_col] * sub_det
-
-    return det
+    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
 
 
 def determinant(matrix):
@@ -42,9 +27,7 @@ def determinant(matrix):
         TypeError: If matrix is not a list of lists
         ValueError: If matrix is not square
     """
-    if type(matrix) is not list:
-        raise TypeError("matrix must be a list of lists")
-    if len(matrix) == 0:
+    if type(matrix) is not list or not matrix:
         raise TypeError("matrix must be a list of lists")
     if any(type(row) is not list for row in matrix):
         raise TypeError("matrix must be a list of lists")
@@ -52,4 +35,12 @@ def determinant(matrix):
         return 1
     if any(len(matrix) != len(row) for row in matrix):
         raise ValueError("matrix must be a square matrix")
-    return(recursive_determinant(matrix))
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0]
+    det = 0
+    for c in range(len(matrix)):
+        det += ((-1)**c)*matrix[0][c] * determinant(matmin(matrix, 0, c))
+    return det
--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -1,1 +1,74 @@
 #!/usr/bin/env python3
+"""
+1-minor module
+contains functions: matmin, determinant and minor
+"""
+
+
+def matmin(m, i, j):
+    """Slices matrix for minor calculation
+    Args:
+        matrix (list): list of lists whose minor should be calculated
+        i (int):
+        j (int):
+    Returns:
+        (list): the sliced matrix for minor calculation
+    """
+    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
+
+
+def determinant(matrix):
+    """Calculates the determinant of a matrix
+    Args:
+        matrix (list): list of lists whose determinant should be calculated
+    Returns:
+        (float): the determinant of matrix
+    Raises:
+        TypeError: If matrix is not a list of lists
+        ValueError: If matrix is not square
+    """
+    if type(matrix) is not list or not matrix:
+        raise TypeError("matrix must be a list of lists")
+    if any(type(row) is not list for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        return 1
+    if any(len(matrix) != len(row) for row in matrix):
+        raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0]
+    det = 0
+    for c in range(len(matrix)):
+        det += ((-1)**c)*matrix[0][c] * determinant(matmin(matrix, 0, c))
+    return det
+
+
+def minor(matrix):
+    """Calculates the minor of a matrix
+    Args:
+        matrix (list): list of lists whose minor should be calculated
+    Returns:
+        (list): the minor matrix of matrix
+    Raises:
+        TypeError: If matrix is not a list of lists
+        ValueError: If matrix is not square or is empty
+    """
+    if type(matrix) is not list or not matrix:
+        raise TypeError("matrix must be a list of lists")
+    if any(type(row) is not list for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if any((not row or len(matrix) != len(row)) for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+    n = len(matrix)
+    if n == 1:
+        return [[1]]
+    min = []
+    for i in range(n):
+        row = []
+        min.append(row)
+        for j in range(n):
+            row.append(determinant(matmin(matrix, i, j)))
+    return min
