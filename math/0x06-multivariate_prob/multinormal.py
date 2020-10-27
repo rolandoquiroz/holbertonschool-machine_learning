d972f5dcc21095a2a7863380ef0a456f92dc4ddc
rolandoquiroz <rolandoquiroz@gmail.com>
2020-10-27 04:54:31 +0000

    Task 3: added

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -34,3 +34,32 @@ class MultiNormal(object):
 
         deviation = data - self.mean
         self.cov = np.matmul(deviation, deviation.T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        Public instance method def pdf that calculates the PDF at a data point
+        Args:
+            x (numpy.ndarray): with shape (d, 1) contains the data point
+            whose PDF should be calculated
+                d is the number of dimensions of the Multinomial instance
+        Returns:
+         The value of the PDF
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError('x must be a numpy.ndarray')
+
+        d = self.cov.shape[0]
+        n, one = x.shape
+        if len(x.shape) is not 2 or n != d or one != 1:
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
+
+        m = self.mean
+        c = self.cov
+
+        cov_inv = np.linalg.inv(c)
+        expo = (-0.5 * np.matmul(np.matmul((x - m).T, cov_inv), x - self.mean))
+
+        pdf = (1 / np.sqrt(((2 * np.pi) ** n) *
+               np.linalg.det(c))) * np.exp(expo[0][0])
+
+        return pdf
