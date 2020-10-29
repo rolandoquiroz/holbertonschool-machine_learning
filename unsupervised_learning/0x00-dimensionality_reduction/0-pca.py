d1a9ea17eec86ca5219ef8fb5ae2a2f01b6ac954
rolandoquiroz <rolandoquiroz@gmail.com>
2020-10-29 21:15:51 +0000

    Task 0: Added

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+module 0-pca
+contains the pca function
+"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """
+    Performs PCA on a dataset
+    Parameters
+    ----------
+    X : numpy.ndarray of shape (n, d) where:
+        n is the number of data points
+        d is the number of dimensions in each point
+    var float :
+        fraction of the variance that the PCA
+        transformation should maintain
+    Returns
+    -------
+    W: numpy.ndarray of shape (d, nd) where:
+        weight matrix that maintains var fraction of X‘s original variance
+        where nd is the new dimensionality of the transformed X
+    """
+    _, s, vh = np.linalg.svd(X)
+    r = np.argmax(np.cumsum(s) > np.sum(s) * var)
+    W = vh[:r + 1].transpose()
+    return W
