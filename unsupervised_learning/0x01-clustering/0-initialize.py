5f1da42b616ceac19c846cf332c44fdfd429af67
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-05 22:44:47 +0000

    Task 0: Added

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -26,10 +26,12 @@ def initialize(X, k):
     """
     if type(X) is not np.ndarray or len(X.shape) is not 2:
         return None
-    if type(k) is not int or k < 1:
+
+    n, d = X.shape
+    if type(k) is not int or not 0 < k < n:
         return None
-    d = X.shape[1]
-    ans = np.random.uniform(low=np.amin(X, axis=0),
-                            high=np.amax(X, axis=0),
-                            size=(k, d))
-    return ans
+
+    centroids = np.random.uniform(low=np.amin(X, axis=0),
+                                  high=np.amax(X, axis=0),
+                                  size=(k, d))
+    return centroids
