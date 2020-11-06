25a8c298ee7d13fdc4923b5c02a41ac9dcd775c5
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-06 21:07:02 +0000

    Task 4: Added

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -11,12 +11,11 @@ def initialize(X, k):
     """Initializes cluster centroids for K-means"""
     if type(X) is not np.ndarray or len(X.shape) is not 2:
         return None, None, None
-    if type(k) is not int or not 1< k < X.shape[0]+1:
+    if type(k) is not int or k < 1 or k >= X.shape[0]:
         return None, None, None
 
     d = X.shape[1]
     pi = np.tile(1/k, (k,))
     m = kmeans(X, k)[0]
     S = np.tile(np.identity(d), (k, 1, 1))
-
     return pi, m, S
