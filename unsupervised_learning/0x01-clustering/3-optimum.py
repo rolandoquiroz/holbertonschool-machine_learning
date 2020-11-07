aa4a1073de666103a3f42e7d12b77498f0ed79ea
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-07 01:19:07 +0000

    Task 3: Attribute error solved

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -10,10 +10,7 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """Tests for the optimum number of clusters by variance"""
-
     if type(X) is not np.ndarray or len(X.shape) is not 2:
-        return None, None
-    if type(iterations) is not int or iterations < 1:
         return None, None
     if kmax is None:
         kmax = X.shape[0]
@@ -23,14 +20,16 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         return None, None
     if kmax <= kmin:
         return None, None
+    if type(iterations) is not int or iterations < 1:
+        return None, None
 
     results = []
     d_vars = []
     for k in range(kmin, kmax + 1):
         C, clss = kmeans(X, k, iterations)
-        results.append((C, clss))
-        if kmin == k:
+        results = results + [(C, clss)]
+        if kmin is k:
             var_min = variance(X, C)
         var = variance(X, C)
-        d_vars.append(var_min - var)
+        d_vars = d_vars + [var_min - var]
     return results, d_vars
