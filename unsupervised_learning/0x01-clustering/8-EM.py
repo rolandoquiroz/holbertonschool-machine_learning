1a9937ffa7de4c83a8ff92455f32b29dd7b634ce
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-07 04:28:09 +0000

    Task 8: Added

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -12,28 +12,34 @@ maximization = __import__('7-maximization').maximization
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """Performs the expectation maximization for a GMM"""
     try:
+        if type(X) is not np.ndarray or len(X.shape) is not 2:
+            return None, None, None, None, None
+        if type(k) is not int or k <= 0 or X.shape[0] < k:
+            return None, None, None, None, None
+        if type(iterations) is not int or iterations <= 0:
+            return None, None, None, None, None
         if type(tol) is not float or tol < 0:
             return None, None, None, None, None
         if type(verbose) is not bool:
             return None, None, None, None, None
-        if iterations < 1:
-            return None, None, None, None, None
 
         pi, m, S = initialize(X, k)
-        g, likelihood = expectation(X, pi, m, S)
-        likelihood_prev = 0
+        prev_likelihood = 0
+
         for i in range(iterations):
-            if i % 10 == 0 and verbose is True:
-                print("Log Likelihood after {} iterations: {}"
-                      .format(i, likelihood))
-            pi, m, S = maximization(X, g)
             g, likelihood = expectation(X, pi, m, S)
-            if np.abs(likelihood - likelihood_prev) <= tol:
-                if verbose is True:
-                    print("Log Likelihood after {} iterations: {}"
-                          .format(i + 1, likelihood))
+            pi, m, S = maximization(X, g)
+            if verbose:
+                message = 'Log Likelihood after {} iterations: {}'\
+                    .format(i, likelihood.round(5))
+                if (i % 10 == 0) or (i == 0):
+                    print(message)
+                if abs(likelihood - prev_likelihood) <= tol:
+                    print(message)
                     break
-            likelihood_prev = likelihood
+            if abs(likelihood - prev_likelihood) <= tol:
+                break
+            prev_likelihood = likelihood
         return pi, m, S, g, likelihood
     except Exception:
         return None, None, None, None, None
