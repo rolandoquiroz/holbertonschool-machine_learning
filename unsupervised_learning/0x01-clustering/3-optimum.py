5fe3e5fd4ba476623e867c82dd20c236923aad9a
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-07 01:52:02 +0000

    Task 3: Attribute error solved

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -11,26 +11,29 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """Tests for the optimum number of clusters by variance"""
-    if type(X) is not np.ndarray or len(X.shape) is not 2:
-        return None, None
-    if kmax is None:
-        kmax = X.shape[0]
-    if type(kmin) is not int or kmin < 1 or kmin >= X.shape[0]:
-        return None, None
-    if type(kmax) is not int or kmax < 1 or kmax > X.shape[0]:
-        return None, None
-    if kmin >= kmax:
-        return None, None
-    if type(iterations) is not int or iterations < 1:
-        return None, None
+    try:
+        if type(X) is not np.ndarray or len(X.shape) is not 2:
+            return None, None
+        if type(iterations) is not int or iterations < 1:
+            return None, None
+        if kmax is None:
+            kmax = X.shape[0]
+        if type(kmin) is not int or kmin < 1 or X.shape[0] <= kmin:
+            return None, None
+        if type(kmax) is not int or kmax < 1 or X.shape[0] < kmax:
+            return None, None
+        if kmin >= kmax:
+            return None, None
 
-    results = []
-    d_vars = []
-    for k in range(kmin, kmax + 1):
-        C, clss = kmeans(X, k, iterations)
-        results = results + [(C, clss)]
-        var = variance(X, C)
-        if k is kmin:
-            minimun_var = var
-        d_vars = d_vars + [minimun_var - var]
-    return results, d_vars
+        results = []
+        d_vars = []
+        for k in range(kmin, kmax + 1):
+            C, clss = kmeans(X, k, iterations)
+            results = results + [(C, clss)]
+            var = variance(X, C)
+            if k == kmin:
+                minimun_var = var
+            d_vars = d_vars + [minimun_var - var]
+        return results, d_vars
+    except Exception:
+        return None, None
