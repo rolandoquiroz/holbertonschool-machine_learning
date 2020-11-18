fd20081cc74b2a2588f3697266ed4c1a0d152f33
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-18 18:45:57 -0500

    Add covariance kernel

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -9,40 +9,40 @@ import numpy as np
 class GaussianProcess:
     """Class that represents a noiseless 1D Gaussian process"""
 
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Class constructor: object attributes initialization
 
-def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-    """
-    Class constructor: object attributes initialization
+        Arguments:
+            X_init: numpy.ndarray of shape (t, 1)
+                representing the inputs already sampled with
+                the black-box function
+            Y_init: numpy.ndarray of shape (t, 1)
+                representing the outputs of the black-box function
+                for each input in X_init
+            t: int
+                the number of initial samples
+            l: int
+                the length parameter for the kernel
+            sigma_f: float
+                the standard deviation given to the output of
+                the black-box function
 
-    Arguments:
-        X_init: numpy.ndarray of shape (t, 1)
-            representing the inputs already sampled with the black-box function
-        Y_init: numpy.ndarray of shape (t, 1)
-            representing the outputs of the black-box function
-            for each input in X_init
-        t: int
-            the number of initial samples
-        l: int
-            the length parameter for the kernel
-        sigma_f: float
-            the standard deviation given to the output of
-            the black-box function
+        Public instance attributes:
+            X: corresponding to the respective constructor input
+            Y: corresponding to the respective constructor input
+            l: corresponding to the respective constructor input
+            sigma_f: corresponding to the respective constructor input
+            K: float
+                Representing the current covariance kernel matrix
+                for the Gaussian process
 
-    Public instance attributes:
-        X: corresponding to the respective constructor input
-        Y: corresponding to the respective constructor input
-        l: corresponding to the respective constructor input
-        sigma_f: corresponding to the respective constructor input
-        K: float
-            Representing the current covariance kernel matrix
-            for the Gaussian process
-
-    """
-    self.X = X_init
-    self.Y = Y_init
-    self.l = l
-    self.sigma_f = sigma_f
-    self.K = self.kernel(X_init, X_init)
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
         """
@@ -54,7 +54,11 @@ def __init__(self, X_init, Y_init, l=1, sigma_f=1):
             X2 is a numpy.ndarray of shape (n, 1)
 
         Returns:
-            numpy.ndarray of shape (m, n)
+            K: numpy.ndarray of shape (m, n)
             The covariance kernel matrix
         """
-        return np.exp(-np.linalg.norm(X1 - X2, ))
+        sqdist = (np.sum(X1 ** 2, 1).reshape(-1, 1) +
+                  np.sum(X2 ** 2, 1) -
+                  2 * np.matmul(X1, X2.T))
+        K = self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+        return K
