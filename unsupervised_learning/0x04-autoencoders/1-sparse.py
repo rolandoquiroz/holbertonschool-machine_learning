295729d6e32d09819f82ad35f69171a9104997a2
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-28 12:10:08 -0500

    Add Task 1: Sparse autoencoder

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -22,8 +22,54 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
                  L1 regularization on the encoded output.
 
     Returns:
-        encoder, decoder, auto: `tuple`,
+        (encoder, decoder, auto): `tuple`,
             encoder is the encoder model.
             decoder is the decoder model.
             auto is the sparse autoencoder model.
     """
+    # This is our input placeholder
+    inputs = keras.Input(shape=(input_dims, ))
+
+    encoder_layers = keras.layers.Dense(units=hidden_layers[0],
+                                        activation='relu')(inputs)
+
+    for layer in hidden_layers[1:]:
+        encoder_layers = keras.layers.Dense(units=layer,
+                                            activation='relu')(encoder_layers)
+
+    act_reg = keras.regularizers.l1(lambtha)
+    # "encoded" is the encoded representation of the input
+    encoded = keras.layers.Dense(units=latent_dims,
+                                 activation='relu',
+                                 activity_regularizer=act_reg)(encoder_layers)
+
+    # This is our encoded input placeholder
+    encoded_inputs = keras.Input(shape=(latent_dims,))
+
+    decoder_layers = keras.layers.Dense(units=hidden_layers[-1],
+                                        activation='relu')(encoded_inputs)
+
+    for layer in reversed(hidden_layers[:-1]):
+        decoder_layers = keras.layers.Dense(units=layer,
+                                            activation='relu')(decoder_layers)
+
+    # "decoded" is the lossy reconstruction of the input
+    decoded = keras.layers.Dense(units=input_dims,
+                                 activation='sigmoid')(decoder_layers)
+
+    # Next model maps an input to its encoded representation
+    # also called latent space representation or code
+    encoder = keras.Model(inputs=inputs, outputs=encoded)
+    # Next model maps a encoded representation
+    # to its lossy reconstruction of the input
+    decoder = keras.Model(inputs=encoded_inputs, outputs=decoded)
+
+    code = encoder(inputs)
+    outputs = decoder(code)
+
+    # This model maps an input to its reconstruction
+    auto = keras.Model(inputs=inputs, outputs=outputs)
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
