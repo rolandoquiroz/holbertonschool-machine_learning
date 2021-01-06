3ec2aa5b76ab58a6107b4230aced1a24c3697565
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-06 11:06:30 -0500

    Feature: Scaled dot product attention

--- supervised_learning/0x11-attention/5-sdp_attention.py
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 def sdp_attention(Q, K, V, mask=None):
     """
     Function that calculates the scaled dot product attention.
+    https://www.tensorflow.org/tutorials/text/transformer#scaled_dot_product_attention
 
     Arguments:
         Q: tensor with its last two dimensions as (..., seq_len_q, dk)
@@ -17,7 +18,6 @@ def sdp_attention(Q, K, V, mask=None):
         mask: tensor that can be broadcast into (..., seq_len_q, seq_len_v)
             the optional mask, defaulted to None
 
-
     Returns: output, weights
         output: tensor with its last two dimensions as (..., seq_len_q, dv)
             the scaled dot product attention
@@ -25,22 +25,16 @@ def sdp_attention(Q, K, V, mask=None):
                                                          seq_len_v)
             the attention weights
     """
-    # (..., seq_len_q, seq_len_k)
-    matmul_QK = tf.matmul(Q, K, transpose_b=True)
+    QKT = tf.matmul(Q, K, transpose_b=True)
 
-    # scale matmul_qk
     dk = tf.cast(tf.shape(K)[-1], tf.float32)
-    scaled_attention_logits = matmul_QK / tf.math.sqrt(dk)
+    scaled_attention_logits = QKT / tf.math.sqrt(dk)
 
-    # add the mask to the scaled tensor.
     if mask is not None:
         scaled_attention_logits += (mask * -1e9)
 
-    # softmax is normalized on the last axis (seq_len_k) so that the scores
-    # add up to 1.
-    # (..., seq_len_q, seq_len_k)
     attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
 
-    output = tf.matmul(attention_weights, V)  # (..., seq_len_q, depth_v)
+    output = tf.matmul(attention_weights, V)
 
     return output, attention_weights
