d1e4d83c86ba536427f65f5d5b4a67f582747caf
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-05 23:33:24 -0500

    Feature: Positional Encoding for a transformer

--- supervised_learning/0x11-attention/4-positional_encoding.py
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -20,6 +20,6 @@ def positional_encoding(max_seq_len, dm):
     pe = np.zeros([max_seq_len, dm])
     for pos in range(max_seq_len):
         for i in range(0, dm, 2):
-            pe[pos, i] = np.sin(pos / (10000 ** ((2 * i)/dm)))
-            pe[pos, i + 1] = np.cos(pos / (10000 ** ((2 * (i + 1))/dm)))
+            pe[pos, i] = np.sin(pos / (10000 ** ((2 * i)//dm)))
+            pe[pos, i + 1] = np.cos(pos / (10000 ** ((2 * (i + 1))//dm)))
     return pe
