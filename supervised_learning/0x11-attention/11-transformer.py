3698463cbad9fca24ca56a4d71f5ffb1191a276c
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-06 20:04:04 -0500

    Feature: Transformer Network

--- supervised_learning/0x11-attention/11-transformer.py
+++ supervised_learning/0x11-attention/11-transformer.py
@@ -1,1 +1,69 @@
 #!/usr/bin/env python3
+"""class Decoder"""
+import tensorflow as tf
+Encoder = __import__('9-transformer_encoder').Encoder
+Decoder = __import__('10-transformer_decoder').Decoder
+
+
+class Transformer(tf.keras.Model):
+    """
+    class Transformer to create a transformer network
+    https://www.tensorflow.org/tutorials/text/transformer
+    """
+
+    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
+                 max_seq_input, max_seq_target, drop_rate=0.1):
+        """
+        class Transformer constructor
+
+        Arguments:
+            N: the number of blocks in the decoder
+            dm: the dimensionality of the model
+            h: the number of heads
+            hidden: the number of hidden units in the fully connected layer
+            input_vocab: the size of the input vocabulary
+            target_vocab: the size of the target vocabulary
+            max_seq_input: the maximum sequence length possible for the input
+            max_seq_target: the maximum sequence length possible for the target
+            drop_rate: the dropout rate
+
+        Public instance attributes:
+            encoder: the encoder layer
+            decoder: the decoder layer
+            linear: a final Dense layer with target_vocab units
+        """
+        super(Transformer, self).__init__()
+        self.encoder = Encoder(N, dm, h, hidden, input_vocab, max_seq_input,
+                               drop_rate)
+        self.decoder = Decoder(N, dml, h, hidden, target_vocab, max_seq_target,
+                               drop_rate)
+        self.linear = tf.keras.layers.Dense(target_vocab)
+
+    def call(self, inputs, target, training, encoder_mask, look_ahead_mask,
+             decoder_mask):
+        """
+        Method to make a 'call' for a Transformer network forward pass.
+        Transformation from inputs to outputs
+
+        Arguments:
+            inputs: tensor of shape (batch, input_seq_len)
+                the inputs
+            target: tensor of shape (batch, target_seq_len)
+                the target
+            training: boolean to determine if the model is training
+            encoder_mask: the padding mask to be applied to the encoder
+            look_ahead_mask: the look ahead mask to be applied to the decoder
+            decoder_mask: the padding mask to be applied to the decoder
+
+        Returns:
+            a tensor of shape (batch, target_seq_len, target_vocab)
+                the transformer output
+        """
+        enc_output = self.encoder(inputs, training, encoder_mask)
+
+        dec_output = self.decoder(target, enc_output, training,
+                                  look_ahead_mask, decoder_mask)
+
+        final_output = self.linear(dec_output)
+
+        return final_output
