9310378247ab2ce35f544a1816d664473a93af87
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-14 11:46:43 -0500

    [Update] Change train and validation load style

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -19,14 +19,13 @@ class Dataset:
             tokenizer_pt: Portuguese tokenizer created from the training set
             tokenizer_en: English tokenizer created from the training set
         """
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='train',
-                                    as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='validation',
-                                    as_supervised=True)
-        tokenizer_pt, tokenizer_en = self.tokenize_dataset(self.data_train)
-        self.tokenizer_pt, self.tokenizer_en = tokenizer_pt, tokenizer_en
+        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                                       with_info=True,
+                                       as_supervised=True)
+        self.data_train, self.data_valid = examples['train'],\
+            examples['validation']
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
 
     def tokenize_dataset(self, data):
         """
@@ -46,11 +45,14 @@ class Dataset:
                 tokenizer_pt is the Portuguese tokenizer
                 tokenizer_en is the English tokenizer
         """
+        target_vocab_size = 2 ** 15
         #              tfds.deprecated.text
         tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data), target_vocab_size=2**15)
+            (pt.numpy() for pt, en in data),
+            target_vocab_size=target_vocab_size)
         #              tfds.deprecated.text
         tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data), target_vocab_size=2**15)
+            (en.numpy() for pt, en in data),
+            target_vocab_size=target_vocab_size)
 
         return tokenizer_pt, tokenizer_en
