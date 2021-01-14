40eee2a560578c2e88ba7f0fa527a14418f77cd9
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-14 12:02:15 -0500

    [FIx] tokenizer_dataset method correct element tokenizer_en

--- supervised_learning/0x12-transformer_apps/2-dataset.py
+++ supervised_learning/0x12-transformer_apps/2-dataset.py
@@ -22,8 +22,8 @@ class Dataset:
         examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
                                        with_info=True,
                                        as_supervised=True)
-        self.data_train = examples['train']
-        self.data_valid = examples['validation']
+        self.data_train, self.data_valid = examples['train'],\
+            examples['validation']
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
         self.data_train = self.data_train.map(self.tf_encode)
@@ -54,7 +54,7 @@ class Dataset:
             target_vocab_size=target_vocab_size)
         #              tfds.deprecated.text
         tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data),
+            (en.numpy() for pt, en in data),
             target_vocab_size=target_vocab_size)
 
         return tokenizer_pt, tokenizer_en
