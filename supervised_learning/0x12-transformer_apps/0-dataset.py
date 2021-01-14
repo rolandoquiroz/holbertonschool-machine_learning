19655841f47ac23d1de5a4c9788ef6556c033d08
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-14 11:36:48 -0500

    [Update] Change train and validation load style

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -20,11 +20,13 @@ class Dataset:
             tokenizer_en: English tokenizer created from the training set
         """
         self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='train', as_supervised=True)
+                                    split='train',
+                                    as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='validation', as_supervised=True)
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)
+                                    split='validation',
+                                    as_supervised=True)
+        tokenizer_pt, tokenizer_en = self.tokenize_dataset(self.data_train)
+        self.tokenizer_pt, self.tokenizer_en = tokenizer_pt, tokenizer_en
 
     def tokenize_dataset(self, data):
         """
