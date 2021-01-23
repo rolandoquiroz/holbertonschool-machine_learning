b0656cb9dd0d1e57ee324b0c5da9d9cb375e2238
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-22 19:16:42 -0500

    Add Task 0

--- supervised_learning/0x13-qa_bot/0-qa.py
+++ supervised_learning/0x13-qa_bot/0-qa.py
@@ -30,6 +30,7 @@ def question_answer(question, reference):
     paragraph_tokens = tokenizer.tokenize(reference)
     tokens = ["[CLS]"] + question_tokens + ["[SEP]"] + paragraph_tokens +\
         ["[SEP]"]
+
     input_word_ids = tokenizer.convert_tokens_to_ids(tokens)
     input_mask = [1] * len(input_word_ids)
     input_type_ids = [0] * (1 + len(question_tokens) + 1) + [1] *\
@@ -38,6 +39,7 @@ def question_answer(question, reference):
     input_word_ids, input_mask, input_type_ids = map(
         lambda t: tf.expand_dims(tf.convert_to_tensor(t, dtype=tf.int32), 0),
         (input_word_ids, input_mask, input_type_ids))
+
     outputs = model([input_word_ids, input_mask, input_type_ids])
     # using `[1:]` will enforce an answer.
     # `outputs[0][0][0]` is the ignored '[CLS]' token logit
