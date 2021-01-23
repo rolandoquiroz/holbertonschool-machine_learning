c336a93707d3f6186493bfccb8720b28e05e9fbd
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-23 00:09:52 -0500

    Add Task 0

--- supervised_learning/0x13-qa_bot/0-qa.py
+++ supervised_learning/0x13-qa_bot/0-qa.py
@@ -49,7 +49,7 @@ def question_answer(question, reference):
     answer_tokens = tokens[short_start: short_end + 1]
     answer = tokenizer.convert_tokens_to_string(answer_tokens)
 
-    if answer:
-        return answer
-    else:
-        return None
+    if not answer:
+        answer = None
+
+    return answer
