8f9c5917211b9059fa84ea18a52a44fdb09be3e0
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-22 23:03:07 -0500

    Add Task 1

--- supervised_learning/0x13-qa_bot/1-loop.py
+++ supervised_learning/0x13-qa_bot/1-loop.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+Script that takes in input from the user with the prompt Q:
+and prints A: as a response. If the user inputs exit, quit,
+goodbye, or bye, case insensitive, print A: Goodbye and exit.
+"""
+question = ""
+while not (question == "exit" or question == "quit" or question == "goodbye"
+           or question == "bye"):
+    print("Q: ", end="")
+    question = input().lower()
+    print("A:")
+else:
+    print("A: Goodbye")
+    exit()
+        
