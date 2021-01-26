9302b08f3d14c86f5c8de9129df612440ed39638
rolandoquiroz <rolandoquiroz@gmail.com>
2021-01-26 14:52:20 -0500

    First commit

--- supervised_learning/0x13-qa_bot/3-semantic_search.py
+++ supervised_learning/0x13-qa_bot/3-semantic_search.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""Function semantic_search"""
+import tensorflow as tf
+import tensorflow_hub as hub
+from transformers import BertTokenizer, TFBertModel
+
+
+def semantic_search(corpus_path, sentence):
+    """
+    Function that performs semantic search on a corpus of documents.
+
+    Parameters
+    ----------
+    corpus_path : str
+        the path to the corpus of reference documents on which to perform
+        semantic search
+    sentence : str
+        the sentence from which to perform semantic search
+
+    Returns
+    -------
+    reference : str
+        the reference text of the document most similar to sentence
+    """
--- supervised_learning/0x13-qa_bot/4-qa.py
+++ supervised_learning/0x13-qa_bot/4-qa.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Function question_answer"""
+import tensorflow as tf
+import tensorflow_hub as hub
+from transformers import BertTokenizer, TFBertModel
+
+
+def question_answer(coprus_path):
+    """
+    Function that answers questions from multiple reference texts.
+
+    Parameters
+    ----------
+    coprus_path : str
+        the path to the corpus of reference documents
+    """
