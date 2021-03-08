a10de0c069289d6ca65487b2a06b084be9661a64
rolandoquiroz <rolandoquiroz@gmail.com>
2021-03-08 16:34:57 -0500

    First commit

---
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""policy gradients"""
+import numpy as numpy
+import gym
+
+
+def policy(matrix, weight):
+    """
+    Function that computes to policy with a weight of a matrix.
+
+    Parameters
+    ----------
+    matrix : [type]
+        [description]
+    weight : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    return a_policy
+
+
+def policy_gradient(state, weight):
+    """
+    [summary]
+
+    Parameters
+    ----------
+    state : [type]
+        matrix representing the current observation of the environment
+    weight : [type]
+        matrix of random weight
+
+    Returns
+    -------
+    action, gradient : tuple
+        [description]
+    """
+    return action, gradient
+
+
---
+++ reinforcement_learning/0x03-policy_gradients/train.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""train"""
+import numpy as numpy
+import matplotlib.pyplot as plt
+import gym
+
+
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+    """
+    [summary]
+
+    Parameters
+    ----------
+    env : [type]
+        initial environment
+    nb_episodes : int
+        number of episodes used for training
+    alpha : float, optional
+        the learning rate, by default 0.000045
+    gamma : float, optional
+        the discount factor, by default 0.98
+    """
+    return
