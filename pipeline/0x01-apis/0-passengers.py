cc7f471b4a19f52567b4c8632cc851a179d50faa
rolandoquiroz <rolandoquiroz@gmail.com>
2021-03-11 12:58:56 -0500

    Add first version files project

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""""""
+
+def availableShips(passengerCount):
+    """
+    Method that returns the list of ships that can hold
+    a given number of passengers from Swapi API.
+
+    Parameters
+    ----------
+    passengerCount : int
+        [description]
+
+    Returns
+    -------
+    ships: list
+        ships that can hold a given number of passengers,
+        If no ship available, return an empty list
+    """
+    return []
---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""""""
+
+def sentientPlanets():
+    """
+    Method that returns the list of names of the home planets
+    of all sentient species using the Swapi API.
+    """
+    return 
