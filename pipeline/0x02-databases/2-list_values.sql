24ad4a98f7a8e2ff09139d379b25ce4adaf8e80d
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-28 09:48:49 -0500

    Add Relational databases tasks

--- pipeline/0x02-databases/2-list_values.sql
+++ pipeline/0x02-databases/2-list_values.sql
@@ -1,4 +1,5 @@
 -- Script that lists all rows of the table first_table in your MySQL server.
 -- All fields should be printed 
 -- The database name is passed as an argument of the mysql command
-SELECT * FROM first_table;
+SELECT *
+FROM first_table;
