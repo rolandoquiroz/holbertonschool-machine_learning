9af1becfc6264b057320ba46f5235f5cf7021fad
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-28 10:55:11 -0500

    Add Relational databases tasks

--- pipeline/0x02-databases/5-average.sql
+++ pipeline/0x02-databases/5-average.sql
@@ -1,4 +1,5 @@
 -- Script that computes the score average of all records in the table second_table in your MySQL server.
--- The result column name is average
--- The database name is passed as an argument of the mysql command
-SELECT AVG(score) AS average FROM second_table;
+--  The result column name is average
+--  The database name is passed as an argument of the mysql command
+SELECT ROUND(AVG(score), 2) AS average
+FROM second_table;
