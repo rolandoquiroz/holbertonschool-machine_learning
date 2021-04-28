563d0fc993699eddd7aa24db02f19bb3d1a3428b
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-28 11:24:36 -0500

    Add Relational databases tasks

--- pipeline/0x02-databases/6-avg_temperatures.sql
+++ pipeline/0x02-databases/6-avg_temperatures.sql
@@ -1,4 +1,7 @@
 -- Script that displays the average temperature (Fahrenheit)
 -- by city ordered by temperature (descending).
 -- Import in hbtn_0c_0 database the temperatures.sql table
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(`value`) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
