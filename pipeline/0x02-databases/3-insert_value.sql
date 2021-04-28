4c788c9580e117b6d07b61cb1c2ed57601d628b5
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-28 10:09:09 -0500

    Add Relational databases tasks

--- pipeline/0x02-databases/3-insert_value.sql
+++ pipeline/0x02-databases/3-insert_value.sql
@@ -1,7 +1,8 @@
 
 -- Script that inserts a new row in the table first_table in your MySQL server.
--- New row:
---  id = 89
---  name = Holberton School
--- The database name is passed as an argument of the mysql command
-INSERT INTO first_table VALUES(89, 'Holberton School');
+--  New row:
+--      id = 89
+--      name = Holberton School
+--  The database name is passed as an argument of the mysql command
+INSERT INTO first_table (id, `name`)
+VALUES(89, 'Holberton School');
