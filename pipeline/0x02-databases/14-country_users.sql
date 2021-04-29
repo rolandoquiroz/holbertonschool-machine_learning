0fc76ad6d17bcdc6c436cdbcbd0081387ee8d3ab
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-29 16:35:25 -0500

    Add relational databases tasks

--- pipeline/0x02-databases/14-country_users.sql
+++ pipeline/0x02-databases/14-country_users.sql
@@ -8,9 +8,9 @@
 -- If the table already exists, this script does not fail
 -- This script can be executed on any database
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255),
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	`name` VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 	PRIMARY KEY (id)
 );
