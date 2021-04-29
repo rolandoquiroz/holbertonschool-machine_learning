a094dca1ad0f642c5f3ab0734062b04416f16b8b
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-29 16:16:20 -0500

    Add relational databases tasks

--- pipeline/0x02-databases/13-uniq_users.sql
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -3,10 +3,11 @@
 --   id, integer, never null, auto increment and primary key
 --   email, string (255 characters), never null and unique
 --   name, string (255 characters)
--- If the table already exists, your script should not fail
--- Your script can be executed on any database
-CREATE TABLE IF NOT EXISTS users(
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	PRIMARY KEY(id));
+-- If the table already exists, this script does not fail
+-- This script can be executed on any database
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    PRIMARY KEY (id)
+);
