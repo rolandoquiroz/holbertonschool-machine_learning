efe94d68765deb38bbfa96c2d9ae54bef1791d7e
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-29 17:12:59 -0500

    Add relational databases tasks

--- pipeline/0x02-databases/15-fans.sql
+++ pipeline/0x02-databases/15-fans.sql
@@ -1,6 +1,11 @@
 -- SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
 -- Requirements:
---  Import the table: metal_bands.sql
---  Column names must be: origin and nb_fans
---  Your script can be executed on any database
-SELECT origin, sum(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
+--      Import table metal_bands.sql
+--      Column names are: origin and nb_fans
+--      This script can be executed on any database
+-- Context: Calculate/compute something is always power intensive… better to distribute the load!
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+HAVING nb_fans > 1
+ORDER BY nb_fans DESC;
