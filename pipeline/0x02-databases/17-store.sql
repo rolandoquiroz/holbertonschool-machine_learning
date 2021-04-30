e3fb80aef241f60f18cae64024f5ae15645b7dad
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-29 22:20:28 -0500

    Add relational databases tasks

--- pipeline/0x02-databases/17-store.sql
+++ pipeline/0x02-databases/17-store.sql
@@ -1,11 +1,14 @@
 -- SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
 -- Quantity in the table items can be negative.
+-- Context: Updating multiple tables for one action from your application can generate issue:
+-- network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!
 delimiter //
-CREATE TRIGGER update_items
-  AFTER INSERT
-  ON orders
-  FOR EACH ROW
+CREATE TRIGGER decrease_the_quantity_of_an_item_after_adding_a_new_order
+AFTER INSERT ON orders
+FOR EACH ROW
 BEGIN
-  update items SET quantity = quantity - new.number WHERE items.name=new.item_name;
-END //
-delimiter ;
+  UPDATE items
+  SET quantity = quantity - NEW.`number`
+  WHERE items.`name` = NEW.item_name;
+END;//
+delimiter;
