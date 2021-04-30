e674fc8bd50b36a8df2fa8b5c3586c84baf86b5c
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-30 09:10:18 -0500

    Add relational databases tasks

--- pipeline/0x02-databases/18-valid_email.sql
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -2,11 +2,11 @@
 -- Context: Nothing related to MySQL, but perfect for user email validation - distribute the logic to the database itself!
 delimiter //
 CREATE TRIGGER reset_valid_email
-	BEFORE UPDATE ON users
-	FOR EACH ROW
-	BEGIN
-  		IF STRCMP(old.email, new.email) != 0 THEN
-			SET new.valid_email = 0;
-		END IF;
-	END;//
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF STRCMP(old.email, new.email) != 0 THEN
+		SET new.valid_email = 0;
+	END IF;
+END;//
 delimiter;
