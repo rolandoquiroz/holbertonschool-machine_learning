c72540599dde40ed329ab2b386f6e58b804aad12
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-29 23:03:38 -0500

    Add relational databases tasks

--- pipeline/0x02-databases/17-store.sql
+++ pipeline/0x02-databases/17-store.sql
@@ -3,7 +3,7 @@
 -- Context: Updating multiple tables for one action from your application can generate issue:
 -- network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!
 delimiter //
-CREATE TRIGGER decrease_the_quantity_of_an_item_after_adding_a_new_order
+CREATE TRIGGER update_items_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
