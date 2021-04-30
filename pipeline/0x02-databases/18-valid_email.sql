96b4ae2405d8c271bf9ca380d50ccf1179d5d958
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-30 09:11:31 -0500

    Add relational databases tasks

--- pipeline/0x02-databases/18-valid_email.sql
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -5,8 +5,8 @@ CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-	IF STRCMP(old.email, new.email) != 0 THEN
-		SET new.valid_email = 0;
+	IF STRCMP(OLD.email, NEW.email) != 0 THEN
+		SET NEW.valid_email = 0;
 	END IF;
 END;//
 delimiter;
