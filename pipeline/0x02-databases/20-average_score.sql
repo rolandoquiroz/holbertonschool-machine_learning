a150e970fcec93551234d8b711aa871ddd47c3b0
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-30 13:00:37 -0500

    Add relational databases tasks

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -7,6 +7,6 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN new_user_id INT)
 BEGIN
 	UPDATE users
-	SET average_score=(SELECT AVG(score) FROM corrections WHERE user_id=new_user_id) WHERE id=new_user_id;
+	SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id=new_user_id) WHERE id=new_user_id;
 END $$
 DELIMITER ;
